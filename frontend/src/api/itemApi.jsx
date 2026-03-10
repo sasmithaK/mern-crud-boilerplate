8489eb15d941279aae997d190d59509a0f3b0569
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-03-10 11:13:43 +0530

    chore: Code base cleanup
    Co-Authored-By: Isara Ekanayake <179592272+isarase@users.noreply.github.com>

--- frontend/src/api/itemApi.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const BASE = '/api/items';
-
-export const getItems   = () => axios.get(BASE);
-export const getItem    = id => axios.get(`${BASE}/${id}`);
-export const createItem = data => axios.post(BASE, data);
-export const updateItem = (id, data) => axios.put(`${BASE}/${id}`, data);
-export const deleteItem = id => axios.delete(`${BASE}/${id}`);
